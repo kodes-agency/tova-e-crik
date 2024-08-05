80a6267b7553cd2b277dd7dd793ad44ba441eaaf
denevB <95876310+denevB@users.noreply.github.com>
2024-08-05 13:17:10 +0300

    origin

--- src/components/next/home/Menu.tsx
+++ src/components/next/home/Menu.tsx
@@ -3,7 +3,7 @@
 import { isMobile } from 'react-device-detect'
 import { Media } from '@/payload-types'
 import { useRouter } from 'next/navigation'
-import Link from 'next/link';
+import Link from 'next/link'
 
 type Props = {
   buttonText: string
@@ -13,19 +13,22 @@ type Props = {
   id?: string | null | undefined
 }
 
-
 export const Menu = ({ menu, index }: { menu: Props; index: number }) => {
   const router = useRouter()
 
   const handleMouseEnter = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    const video = document.querySelector(`video[data-video="${menu.buttonLink}"]`) as HTMLVideoElement
+    const video = document.querySelector(
+      `video[data-video="${menu.buttonLink}"]`,
+    ) as HTMLVideoElement
     if (video) {
       video.play()
     }
   }
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    const video = document.querySelector(`video[data-video="${menu.buttonLink}"]`) as HTMLVideoElement
+    const video = document.querySelector(
+      `video[data-video="${menu.buttonLink}"]`,
+    ) as HTMLVideoElement
     if (video) {
       video.currentTime = 0
       video.play()
@@ -33,47 +36,55 @@ export const Menu = ({ menu, index }: { menu: Props; index: number }) => {
   }
 
   const handleMouseLeave = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-    const video = document.querySelector(`video[data-video="${menu.buttonLink}"]`) as HTMLVideoElement
+    const video = document.querySelector(
+      `video[data-video="${menu.buttonLink}"]`,
+    ) as HTMLVideoElement
     if (video) {
       video.pause()
     }
   }
 
-  if(isMobile) {
-    <div key={index} className="w-full flex flex-col items-center">
-    <button 
-      className='font-black peer text-center lowercase text-6xl py-2 relative z-10 w-full hover:invert hover:mix-blend-difference'
-      data-video={menu.buttonLink}
-      onMouseEnter={handleClick}
-    >{menu.buttonText}</button>
-    <video
-      // @ts-expect-error
-      src={menu.thumbnail.url} alt={menu.thumbnail.alt}
-      className="fixed top-0 left-0 w-full object-cover h-full pointer-events-none z-0 opacity-0 peer-hover:opacity-100 transition-opacity duration-300 ease-in-out"
-      data-video={menu.buttonLink}
-      muted
-      // onEnded={()=>{
-      //   router.push(menu.buttonLink)
-      // }}
-      disablePictureInPicture
-      disableRemotePlayback
-      playsInline
-      preload="auto"
-    >
-      {/* @ts-ignore */}
-       <source src={menu.thumbnail.url} type="video/webm"/>
-    </video>
-  </div>
+  if (isMobile) {
+    return (
+      <div key={index} className="w-full flex flex-col items-center">
+        <button
+          className="font-black peer text-center lowercase text-6xl py-2 relative z-10 w-full hover:invert hover:mix-blend-difference"
+          data-video={menu.buttonLink}
+          onClick={handleClick}
+        >
+          M{menu.buttonText}
+        </button>
+        <video
+          // @ts-expect-error
+          src={menu.thumbnail.url} alt={menu.thumbnail.alt}
+          className="fixed top-0 left-0 w-full object-cover h-full pointer-events-none z-0 opacity-0 peer-hover:opacity-100 transition-opacity duration-300 ease-in-out"
+          data-video={menu.buttonLink}
+          muted
+          // onEnded={()=>{
+          //   router.push(menu.buttonLink)
+          // }}
+          disablePictureInPicture
+          disableRemotePlayback
+          playsInline
+          preload="auto"
+        >
+          {/* @ts-ignore */}
+          <source src={menu.thumbnail.url} type="video/webm" />
+        </video>
+      </div>
+    )
   } else {
     return (
       <div key={index} className="w-full flex flex-col items-center">
-        <Link 
-          className='font-black peer text-center lowercase text-6xl py-2 relative z-10 w-full hover:invert hover:mix-blend-difference'
+        <Link
+          className="font-black peer text-center lowercase text-6xl py-2 relative z-10 w-full hover:invert hover:mix-blend-difference"
           href={menu.buttonLink}
           data-video={menu.buttonLink}
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
-        >{menu.buttonText}</Link>
+        >
+          D{menu.buttonText}
+        </Link>
         <video
           // @ts-expect-error
           src={menu.thumbnail.url} alt={menu.thumbnail.alt}
@@ -87,11 +98,9 @@ export const Menu = ({ menu, index }: { menu: Props; index: number }) => {
           preload="auto"
         >
           {/* @ts-ignore */}
-           <source src={menu.thumbnail.url} type="video/webm"/>
+          <source src={menu.thumbnail.url} type="video/webm" />
         </video>
       </div>
     )
   }
-
 }
-
